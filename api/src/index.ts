b66edd6d5708c798853c4fb36f7cd721fa9774c9
Peter Moss <peter@moss.dk>
2022-10-28 11:46:17 +0200

    fix cors related issues

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,11 @@
 import * as express from 'express'
+import * as cors from 'cors'
 import router from './routes'
 
 const app = express()
 const port = 3000
 
+app.use(cors())
 app.use(router)
 
 app.listen(port, () => console.log(`listening on port ${port}`))
